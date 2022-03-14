e0eafbf5e1e7fc44bb05e7a43885717be853adb9
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2022-03-14 10:48:18 -0300

    solucao muito ruim

---
+++ src/model/entities/Reservation.java
@@ -0,0 +1,16 @@
+package model.entities;
+
+public class Reservation {
+    private Integer roomNumber;
+    private Date checkIn;
+    private Date checkOut
+
+            // Metodo
+    public Integer duration() {
+        return null;
+    }
+    public void updateDates(Date checkIn, Date checkIn) {
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+}
